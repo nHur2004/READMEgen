a0974586a2121aa031920ce7edbad639bebc5452
nHur2004 <nathan.hur04@gmail.com>
2022-12-18 13:54:39 -0700

    license array

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const projectInput = () => {
     return inquirer.createPromptModule([
-        {
+        { // Title Input
             type: 'input',
             name: 'title',
             message: 'What is your project title? (Required)',
@@ -19,7 +19,7 @@ const projectInput = () => {
                 }
             }
         },
-        {
+        { // Desc Input
             type: 'input',
             name: 'description',
             message: 'Provide a description of the project. (Required)',
@@ -32,7 +32,7 @@ const projectInput = () => {
                 }
             }
         },
-        {
+        { // Install Input
             type: 'input',
             name: 'installation',
             message: 'Describe how to install this project. (Required)',
@@ -45,7 +45,7 @@ const projectInput = () => {
                 }
             }
         },
-        {
+        { // Usage Input
             type: 'input',
             name: 'usage',
             message: 'How do you use this project? (Required)',
@@ -58,7 +58,7 @@ const projectInput = () => {
                 }
             }
         },
-        {
+        { // Contribution Input
             type: 'confirm',
             name: 'confirmContribution',
             message: 'Would you like to add contribution guidelines?',
@@ -70,7 +70,7 @@ const projectInput = () => {
             message: 'Explain guidelines for contributions to this project.',
             when: ({ confirmContribution }) => confirmContribution
         },
-        {
+        { // Tests Input
             type: 'confirm',
             name: 'confirmTest',
             message: 'Would you like to add test instructions?',
@@ -81,6 +81,47 @@ const projectInput = () => {
             name: 'test',
             message: 'Explain how to run tests for this application.',
             when: ({ confirmTest }) => confirmTest
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your project. (Required)',
+            choices: [
+                "Academic Free License v3.0",
+                "Apache license 2.0",
+                "Artistic license 2.0",
+                "Boost Software License 1.0",
+                "BSD 2-clause 'Simplified' license",
+                "BSD 3-clause 'New' or 'Revised' license",
+                "BSD 3-clause Clear license",
+                "Creative Commons license family",
+                "Creative Commons Zero v1.0 Universal",
+                "Creative Commons Zero v1.0 Universal",
+                "Creative Commons Attribution Share Alike 4.0",
+                "Do What The F*ck You Want To Public License",
+                "Educational Community License v2.0",
+                "Eclipse Public License 1.0",
+                "Eclipse Public License 2.0",
+                "European Union Public License 1.1",
+                "GNU Affero General Public License v3.0",
+                "GNU General Public License family",
+                "GNU General Public License v2.0",
+                "GNU General Public License v3.0",
+                "GNU Lesser General Public License family",
+                "GNU Lesser General Public License v2.1",
+                "GNU Lesser General Public License v3.0",
+                "ISC",
+                "LaTeX Project Public License v1.3c",
+                "Microsoft Public License",
+                "MIT",
+                "Mozilla Public License 2.0",
+                "Open Software License 3.0",
+                "PostgreSQL License",
+                "SIL Open Font License 1.1",
+                "University of Illinois/NCSA Open Source License",
+                "The Unlicense",
+                "zLib License"
+            ]
         }
     ])
 };
